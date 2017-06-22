465272064aa81b674362bd947d454f6260e7ca01
Dat Phan <datphan0110@gmail.com>
2017-06-22 13:50:19 +0700

    rebuild

--- scripts/deploy_gcp.sh
+++ scripts/deploy_gcp.sh
@@ -18,14 +18,14 @@ gcloud config set compute/zone $GCP_ZONE
 #gcloud --verbosity debug app deploy app.yaml --image-url $GCP_IMAGE --stop-previous-version --quiet
 
 # for GKE
-gcloud container clusters get-credentials $GCP_CLUSTER_NAME
+gcloud container clusters get-credentials $GCP_CLUSTER_NAME --zone $GCP_ZONE --project $GCP_PROJECT_ID
 
 #kubectl delete secret registry.gitlab.com
 #kubectl create secret docker-registry registry.gitlab.com --docker-server=$CI_REGISTRY --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
 kubectl version
 
-kubectl apply -f app.yaml
+# kubectl apply -f deployment.yaml
 
-# helm version
+helm version
 
-# helm upgrade $HELM_RELEASE_NAME --set image.repository=$HELM_IMAGE_REPOSITORY,image.tag=$HELM_IMAGE_TAG,image.pullPolicy=$HELM_IMAGE_PULL_POLICY,image.lastDeployed=$HELM_IMAGE_LAST_DEPLOYED --install helm-charts/damp-starter
+helm upgrade $HELM_RELEASE_NAME --set image.repository=$HELM_IMAGE_REPOSITORY,image.tag=$HELM_IMAGE_TAG,image.pullPolicy=$HELM_IMAGE_PULL_POLICY,image.lastDeployed=$HELM_IMAGE_LAST_DEPLOYED --install helms/damp-starter
